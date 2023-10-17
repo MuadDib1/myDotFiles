6c49a60caefd581b53e4b54853d96aae58004df6
Yuxuan Xie <yushuan_shieh@yahoo.com>
2023-10-17 21:56:20 +0900

    init.emacs modified according to company profile

--- emacs/init.el
+++ emacs/init.el
@@ -1,3 +1,4 @@
+;; Code:
 ;; (setq inhibit-startup-message t)
 
 (scroll-bar-mode -1)        ; Disable visible scrollbar
@@ -6,11 +7,21 @@
 (set-fringe-mode 10)        ; Give some breathing room
 (menu-bar-mode -1)            ; Disable the menu bar
 
+;; Set up the visible bell
+(setq visible-bell t)
+
 (setq make-backup-files nil)  ; disable emacs automatic backup~ file
 (setq create-lockfiles nil)   ; disable lock files
 
-;; Set up the visible bell
-(setq visible-bell t)
+(column-number-mode)
+(global-display-line-numbers-mode t)
+
+;; Disable line numbers for some modes
+(dolist (mode '(org-mode-hook
+                term-mode-hook
+                shell-mode-hook
+                eshell-mode-hook))
+  (add-hook mode (lambda () (display-line-numbers-mode 0))))
 
 (set-face-attribute 'default nil :font "SauceCodePro NFM-11")
 (load-theme 'gruvbox t)
@@ -20,3 +31,38 @@
 (add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 (package-initialize)
 
+;; Initialize use-package on non-Linux platforms
+(unless (package-installed-p 'use-package)
+   (package-install 'use-package))
+
+(require 'use-package)
+(setq use-package-always-ensure t)
+
+;; Org mode
+(require 'org)
+
+;; Evil mode
+(require 'evil)
+(evil-mode 1)
+
+;; flycheck and js2-mode for JavaScript
+(add-hook 'after-init-hook #'global-flycheck-mode)
+(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+(add-hook 'js2-mode-hook
+          (lambda ()
+             (setq my-js-mode-indent-num 2)
+             (setq js2-basic-offset my-js-mode-indent-num)
+             (setq js-switch-indent-offset my-js-mode-indent-num)
+            ))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(flycheck js2-mode gruvbox-theme evil)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
